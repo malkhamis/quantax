b4ed2768c76e587bd28ae6ff9c917a1b08f7057c
Mohammad Alkhamis <malkhamis@protonmail.com>
2019-03-18 22:32:46 -0700

    Add example and test to factory

---
+++ factory/example_test.go
@@ -0,0 +1,61 @@
+package factory
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/malkhamis/quantax/calc"
+	"github.com/malkhamis/quantax/history"
+)
+
+func ExampleNewIncomeTaxCalculatorAgg_Calc() {
+
+	taxParamsFed := IncomeTaxParams{
+		Year:   2018,
+		Region: history.Canada,
+	}
+
+	taxParamsProv := IncomeTaxParams{
+		Year:   2018,
+		Region: history.BC,
+	}
+
+	finNums := calc.FinancialNumbers{TaxableAmount: 170000.0}
+
+	c, err := NewIncomeTaxCalculatorAgg(finNums, taxParamsProv, taxParamsFed)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	aggTax := c.Calc()
+	fmt.Printf("%.2f", aggTax) // Output: 52819.71
+}
+
+func ExampleNewIncomeTaxCalculatorAgg_Update() {
+
+	taxParamsFed := IncomeTaxParams{
+		Year:   2018,
+		Region: history.Canada,
+	}
+
+	taxParamsProv := IncomeTaxParams{
+		Year:   2018,
+		Region: history.BC,
+	}
+
+	finNums := calc.FinancialNumbers{TaxableAmount: 170000.0}
+
+	c, err := NewIncomeTaxCalculatorAgg(finNums, taxParamsProv, taxParamsFed)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	newFinNums := calc.FinancialNumbers{TaxableAmount: 20000.0}
+	c.Update(newFinNums)
+
+	aggTax := c.Calc()
+	fmt.Printf("%.2f", aggTax) // Output: 1713.80
+
+}
