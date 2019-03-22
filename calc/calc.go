c5d6c8f53ce90a231b90f85ec2e1579a27a896e6
Mohammad Alkhamis <malkhamis@protonmail.com>
2019-03-21 22:45:42 -0700

    Enhance the abstraction and clarity of the interfaces

--- calc/calc.go
+++ calc/calc.go
@@ -1,20 +1,47 @@
 // Package calc defines interfaces for various tax-related calculators
 package calc
 
+// Formula represents a method for doing calculations on numeric parameters
+type Formula interface {
+	// Apply applies the formula on numeric parameters and returns the results
+	Apply() float64
+	// Validate checks if the formula is valid for use
+	Validate() error
+}
+
+// TaxFormula represents a method for calculating tax on income
+type TaxFormula interface {
+	// Update sets the financial numbers which the tax will be calculated for.
+	// Users may call this method to set the financial numbers to anything other
+	// than what the tax formula was initialized with
+	Update(IndividualFinances)
+	Formula
+}
+
+// ChildBenefitFormula represents a method for calculating child benefits
+type ChildBenefitFormula interface {
+	// Update sets the financial numbers which benefits will be calculated for.
+	// Users may call this method to set the financial numbers to anything other
+	// than what the child benefit formula was initialized with
+	Update(FamilyFinances)
+	Formula
+}
+
 // TaxCalculator is used to calculate payable tax. Constructors for types
 // implementing this interface should typically accept 'Finances' as argument
 type TaxCalculator interface {
 	// Calc returns the payable amount of tax set/initialized in this calculator
-	Calc() float64
-	// Update sets the financial numbers which the tax will be calculated for in
-	// subsequent calls to Calc(). Users may call this method to set the financial
-	// numbers to anything other than what the calculator was initialized with
-	Update(Finances)
+	Calc(taxCredits ...float64) float64
+	// UpdateFinances sets the financial numbers which the tax will be calculated
+	// for in subsequent calls to Calc(). Users may call this method to set the
+	// financial numbers to anything other than what the calculator was
+	// initialized with
+	UpdateFinances(IndividualFinances)
+	// UpdateFormula sets the tax formula used in this calculator. Users may call
+	// this method to set the formula to anything other than what the calculator
+	// was initialized with
+	UpdateFormula(TaxFormula)
 }
-
-// ChildBenefitFormula is a formula used to calculate the child benefit amount
-// from the given family finances and children
-type ChildBenefitFormula func(FamilyFinances, Children) float64
 
 // ChildBenefitCalculator is used to calculate recievable child benefits for
 // families with dependent children. Constructors for types implementing this
@@ -29,9 +56,9 @@ type ChildBenefitCalculator interface {
 	// UpdateBeneficiary sets the child which the calculator will use in
 	// subsequent calls to Calc(). Users may call this method to set beneficiary
 	// to anything other than what the calculator was initialized with
-	UpdateBeneficiaries(Children)
-	// UpdateRule sets the rule for calculating the amount of benefits for child
-	// given family finances. Users may call this method to set the rule to
-	// anything other than what the calculator was initialized with
+	UpdateBeneficiaries([]Person)
+	// UpdateForumla sets the formula for calculating the amount of benefits for
+	// children given family finances. Users may call this method to set the
+	// formula to anything other than what the calculator was initialized with
 	UpdateForumla(ChildBenefitFormula)
 }
