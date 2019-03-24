ebd8851f93e6bcbd186e6171b036abcfbd0c067c
Mohammad Alkhamis <malkhamis@protonmail.com>
2019-03-24 13:41:22 -0700

    Remove unnecessary handling of bracket boundaries

--- history/bc.go
+++ history/bc.go
@@ -10,10 +10,10 @@ var taxFormulasBC = yearlyTaxFormulas{
 	2018: calc.WeightedBracketFormula{
 		-0.0506: calc.Bracket{0, 10412},
 		0.0506:  calc.Bracket{0, 39676},
-		0.0770:  calc.Bracket{39677, 79353},
-		0.1050:  calc.Bracket{79354, 91107},
-		0.1229:  calc.Bracket{91108, 110630},
-		0.1470:  calc.Bracket{110631, 150000},
-		0.1680:  calc.Bracket{150001, math.Inf(1)},
+		0.0770:  calc.Bracket{39676, 79353},
+		0.1050:  calc.Bracket{79353, 91107},
+		0.1229:  calc.Bracket{91107, 110630},
+		0.1470:  calc.Bracket{110630, 150000},
+		0.1680:  calc.Bracket{150000, math.Inf(1)},
 	},
 }
--- history/canada.go
+++ history/canada.go
@@ -20,10 +20,10 @@ var (
 var taxFormulaCanada2018 = calc.WeightedBracketFormula{
 	-0.150: calc.Bracket{0, 11809},
 	0.150:  calc.Bracket{0, 46605},
-	0.205:  calc.Bracket{46606, 93208},
-	0.260:  calc.Bracket{93209, 144489},
-	0.290:  calc.Bracket{144490, 205842},
-	0.330:  calc.Bracket{205843, math.Inf(1)},
+	0.205:  calc.Bracket{46605, 93208},
+	0.260:  calc.Bracket{93208, 144489},
+	0.290:  calc.Bracket{144489, 205842},
+	0.330:  calc.Bracket{205842, math.Inf(1)},
 }
 
 var cbFormulaCanada2018 = &benefits.CCBFormula{
@@ -40,28 +40,28 @@ var cbFormulaCanada2018 = &benefits.CCBFormula{
 	Reducers: &benefits.StepReducer{
 		StepFormulas: []calc.WeightedBracketFormula{
 			calc.WeightedBracketFormula{ // no children
-				0.000: calc.Bracket{0.00, math.Inf(1)},
+				0.000: calc.Bracket{0, math.Inf(1)},
 			},
 			calc.WeightedBracketFormula{ // 1 child
-				0.000: calc.Bracket{0.00, 30450.00},
-				0.070: calc.Bracket{30450.01, 65976.00},
-				0.032: calc.Bracket{65976.01, math.Inf(1)},
+				0.000: calc.Bracket{0, 30450},
+				0.070: calc.Bracket{30450, 65976},
+				0.032: calc.Bracket{65976, math.Inf(1)},
 			},
 			calc.WeightedBracketFormula{ // 2 children
-				0.000: calc.Bracket{0.00, 30450.00},
-				0.135: calc.Bracket{30450.01, 65976.00},
-				0.057: calc.Bracket{65976.01, math.Inf(1)},
+				0.000: calc.Bracket{0, 30450},
+				0.135: calc.Bracket{30450, 65976},
+				0.057: calc.Bracket{65976, math.Inf(1)},
 			},
 			calc.WeightedBracketFormula{ // 3 children
-				0.000: calc.Bracket{0.00, 30450.00},
-				0.190: calc.Bracket{30450.01, 65976.00},
-				0.080: calc.Bracket{65976.01, math.Inf(1)},
+				0.000: calc.Bracket{0, 30450},
+				0.190: calc.Bracket{30450, 65976},
+				0.080: calc.Bracket{65976, math.Inf(1)},
 			},
 		},
 		AboveMaxStepFormula: calc.WeightedBracketFormula{ // 4+ children
-			0.000: calc.Bracket{0.00, 30450.00},
-			0.230: calc.Bracket{30450.01, 65976.00},
-			0.095: calc.Bracket{65976.01, math.Inf(1)},
+			0.000: calc.Bracket{0, 30450},
+			0.230: calc.Bracket{30450, 65976},
+			0.095: calc.Bracket{65976, math.Inf(1)},
 		},
 	},
 }
