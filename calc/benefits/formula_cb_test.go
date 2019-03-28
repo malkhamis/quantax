05ddf06e2578dc737c046749deb3fae2460f8748
Mohammad Alkhamis <malkhamis@protonmail.com>
2019-03-27 19:50:41 -0700

    Add unit tests for ccbMaxReducer apply

--- calc/benefits/formula_cb_test.go
+++ calc/benefits/formula_cb_test.go
@@ -1,11 +1,75 @@
 package benefits
 
 import (
+	"math"
 	"testing"
 
 	"github.com/malkhamis/quantax/calc"
 	"github.com/pkg/errors"
 )
+
+func TestStepReducer_Apply(t *testing.T) {
+
+	childCount1 := calc.WeightedBracketFormula{
+		0.000: calc.Bracket{0, 10000},
+		0.030: calc.Bracket{10000, 50000},
+		0.070: calc.Bracket{50000, math.Inf(1)},
+	}
+	childCount2 := calc.WeightedBracketFormula{
+		0.000: calc.Bracket{0, 10000},
+		0.050: calc.Bracket{10000, 50000},
+		0.100: calc.Bracket{50000, math.Inf(1)},
+	}
+
+	mr := &CCBMaxReducer{
+		Reducers: []calc.WeightedBracketFormula{childCount1, childCount2},
+		BenefitClasses: []AgeGroupBenefits{
+			{
+				AgesMonths:      calc.AgeRange{0, 11},
+				AmountsPerMonth: calc.Bracket{0, 500},
+			},
+			{
+				AgesMonths:      calc.AgeRange{12, 23},
+				AmountsPerMonth: calc.Bracket{0, 250},
+			},
+		},
+	}
+
+	child1, child2 := calc.Person{AgeMonths: 0}, calc.Person{AgeMonths: 6}
+	max := (12.0 * 500) + (6.0*500 + 6.0*250)
+
+	income := 100000.0
+	expected := max - (0.050 * 40000) - (0.100 * 50000)
+	actual := mr.Apply(income, child1, child2)
+	if actual != expected {
+		t.Errorf("unexpected results\nwant: %.2f\n got: %.2f", expected, actual)
+	}
+
+	income = 100000.0
+	expected = 0.0
+	actual = mr.Apply(income)
+	if actual != expected {
+		t.Errorf("unexpected results\nwant: %.2f\n got: %.2f", expected, actual)
+	}
+
+	income = 500000.0
+	expected = 0.0
+	actual = mr.Apply(income, child1)
+	if actual != expected {
+		t.Errorf("unexpected results\nwant: %.2f\n got: %.2f", expected, actual)
+	}
+	// actualReduction = sr.Reduce(100000, -1.0)
+	// expectedReduction = 0.070*(65976-30450) + 0.032*(100000-65976)
+	// if actualReduction != expectedReduction {
+	// 	t.Errorf("unexpected results\nwant: %.2f\n got: %.2f", expectedReduction, actualReduction)
+	// }
+	//
+	// actualReduction = sr.Reduce(100000, 100.0)
+	// expectedReduction = 0.230*(65976-30450) + 0.095*(100000-65976)
+	// if actualReduction != expectedReduction {
+	// 	t.Errorf("unexpected results\nwant: %.2f\n got: %.2f", expectedReduction, actualReduction)
+	// }
+}
 
 func TestCCBMaxReducer_MinAnnualAmount(t *testing.T) {
 
@@ -33,7 +97,7 @@ func TestCCBMaxReducer_MinAnnualAmount(t *testing.T) {
 
 }
 
-func TestMaxReducer_MaxAnnualAmount(t *testing.T) {
+func TestCCBMaxReducer_MaxAnnualAmount(t *testing.T) {
 
 	formula := CCBMaxReducer{
 		BenefitClasses: []AgeGroupBenefits{
@@ -102,3 +166,33 @@ func TestCCBMaxReducer_Validate_InvalidFormula(t *testing.T) {
 		t.Fatalf("unexpected error\nwant: %v\n got: %v", calc.ErrBoundsReversed, err)
 	}
 }
+
+//
+// func TestCCBMaxReducer_Clone(t *testing.T) {
+//
+// 	formula := &CCBMaxReducer{
+// 		BenefitClasses: []AgeGroupBenefits{
+// 			{
+// 				AgesMonths:      calc.AgeRange{0, 10},
+// 				AmountsPerMonth: calc.Bracket{50, 100},
+// 			},
+// 			{
+// 				AgesMonths:      calc.AgeRange{11, 20},
+// 				AmountsPerMonth: calc.Bracket{25, 50},
+// 			},
+// 		},
+// 	}
+//
+// 	originalResults := formula.Apply(50000, children)(calc.Person{AgeMonths: 5})
+//
+// 	clone := formula.Clone()
+// 	formula.BenefitClasses = nil
+// 	formula.Reducers = nil
+//
+// 	clone.Apply(income, children)
+// 	if actual != expected {
+// 		t.Errorf(
+// 			"expected a 5 month old child to be entitled to %.2f, got %.2f",
+// 			expected, actual,
+// 		)
+// 	}
