e5d1d89189fce3e58b19bf4603f1062bf03bd2d2
Mohammad Alkhamis <malkhamis@protonmail.com>
2019-03-27 22:04:53 -0700

    Add tests for package history

--- history/init.go
+++ history/init.go
@@ -1,8 +1,57 @@
 package history
 
+import "github.com/pkg/errors"
+
 func init() {
 
-	err := validateAllFormulas()
+	err := validateAllTaxFormulas()
+	panicIfError(errors.Wrap(err, "invalid tax formula"))
+
+	err = validateAllChildBenefitFormulas()
+	panicIfError(errors.Wrap(err, "invalid child benefit formula"))
+}
+
+func validateAllTaxFormulas() error {
+
+	for jursdiction, formulasAllYears := range taxFormulasAll {
+		for year, formula := range formulasAllYears {
+
+			if formula == nil {
+				return errors.New("history must not contain nil formulas")
+			}
+
+			err := formula.Validate()
+			if err != nil {
+				return errors.Wrapf(err, "jurisdiction %q, year %d", jursdiction, year)
+			}
+
+		}
+	}
+
+	return nil
+}
+
+func validateAllChildBenefitFormulas() error {
+
+	for jursdiction, formulasAllYears := range cbFormulasAll {
+		for year, formula := range formulasAllYears {
+
+			if formula == nil {
+				return errors.New("history must not contain nil formulas")
+			}
+
+			err := formula.Validate()
+			if err != nil {
+				return errors.Wrapf(err, "jurisdiction %q, year %d", jursdiction, year)
+			}
+
+		}
+	}
+
+	return nil
+}
+
+func panicIfError(err error) {
 	if err != nil {
 		panic(err)
 	}
