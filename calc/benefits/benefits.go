d4dfc50443f0ed3f7801fc36f5bf0138c30e45a9
Mohammad Alkhamis <malkhamis@protonmail.com>
2019-03-27 20:59:09 -0700

    Move income type to a separate file

--- calc/benefits/benefits.go
+++ calc/benefits/benefits.go
@@ -4,29 +4,6 @@ package benefits
 
 import "github.com/malkhamis/quantax/calc"
 
-//go:generate stringer -type=IncomeType -output=incometype_gen.go
-type IncomeType uint
-
-const (
-	REGU IncomeType = iota // regular income (income - deductions)
-	AFNI                   // adjusted family net income (income - deductions - UCCB - RDSP)
-)
-
-// Calc calculates the income according to the underlying method
-// TODO fix this
-func (it IncomeType) Calc(finances calc.FamilyFinances) float64 {
-
-	switch it {
-	case AFNI, REGU:
-		fallthrough
-	default:
-		// this is not exactly right as we should incorporate
-		// UCCB and other shit that I don't care about for now
-		return finances.Income() - finances.Deductions()
-	}
-
-}
-
 // ChildBenefitFormula represents a method for calculating child benefits
 type ChildBenefitFormula interface {
 	// Apply returns the sum of benefits for all beneficiaries
