2190ff9d7431d5816980296d0b3cf922c88bef4f
Mohammad Alkhamis <malkhamis@protonmail.com>
2019-03-27 21:03:11 -0700

    Add tests for income type stringifier

---
+++ calc/benefits/income_type_test.go
@@ -0,0 +1,22 @@
+package benefits
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestIncomeType_String(t *testing.T) {
+
+	if AFNI.String() != "AFNI" {
+		t.Error("unexpected stringified iota")
+	}
+
+	actualStr := IncomeType(10000).String()
+	if !strings.Contains(actualStr, "10000") {
+		t.Errorf(
+			"unexpected strignfied income type\nwant: %s\n got: %s",
+			"IncomeType(10000)", actualStr,
+		)
+	}
+
+}
