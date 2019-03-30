d92e9216c243925cb0e7aba9445288fc5a572d21
Mohammad Alkhamis <malkhamis@protonmail.com>
2019-03-30 01:16:53 -0700

    Add the RRSPCalculator interface

--- calc/calc.go
+++ calc/calc.go
@@ -6,7 +6,7 @@ import (
 	"github.com/malkhamis/quantax/calc/human"
 )
 
-// TaxCalculator is used to calculate payable tax.
+// TaxCalculator is used to calculate payable tax on individual earnings
 type TaxCalculator interface {
 	// Calc returns the payable amount of tax on the income in this calculator
 	Calc(finance.IndividualFinances) float64
@@ -21,4 +21,17 @@ type ChildBenefitCalculator interface {
 	// SetBeneficiaries sets the children which the calculator will compute the
 	// benefits for in subsequent calls to Calc()
 	SetBeneficiaries(...human.Person)
+}
+
+// RRSPCalculator is used to calculate recievable or payable tax on transactions
+// related to Registered Retirement Saving Plan (RRSP) accounts
+type RRSPCalculator interface {
+	// TaxPaid calculates the tax payable upon withdrawal
+	TaxPaid(withdrawal float64) float64
+	// TaxRefund calculates the refundable tax upon deposit/contribution
+	TaxRefund(contribution float64) (float64, error)
+	// ContributionEarned calculates the newly acquired contribution room
+	ContributionEarned() float64
+	// SetFinances makes subsequent calculations based on the given finances
+	SetFinances(finance.IndividualFinances)
 }
