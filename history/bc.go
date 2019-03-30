1a9e2a8ffc4fa070c288e76bc08551a11da85622
Mohammad Alkhamis <malkhamis@protonmail.com>
2019-03-30 00:26:05 -0700

    Fix package history

--- history/bc.go
+++ history/bc.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/malkhamis/quantax/calc"
 	"github.com/malkhamis/quantax/calc/benefits"
+	"github.com/malkhamis/quantax/calc/finance"
 	"github.com/malkhamis/quantax/calc/tax"
 )
 
@@ -19,23 +20,23 @@ var (
 )
 
 var taxFormulaBC2018 = tax.CanadianFormula{
-	-0.0506: calc.Bracket{0, 10412},
-	0.0506:  calc.Bracket{0, 39676},
-	0.0770:  calc.Bracket{39676, 79353},
-	0.1050:  calc.Bracket{79353, 91107},
-	0.1229:  calc.Bracket{91107, 110630},
-	0.1470:  calc.Bracket{110630, 150000},
-	0.1680:  calc.Bracket{150000, math.Inf(1)},
+	-0.0506: finance.Bracket{0, 10412},
+	0.0506:  finance.Bracket{0, 39676},
+	0.0770:  finance.Bracket{39676, 79353},
+	0.1050:  finance.Bracket{79353, 91107},
+	0.1229:  finance.Bracket{91107, 110630},
+	0.1470:  finance.Bracket{110630, 150000},
+	0.1680:  finance.Bracket{150000, math.Inf(1)},
 }
 
 var cbFormulaBC2018 = &benefits.BCECTBMaxReducer{
 	BeneficiaryClasses: []benefits.AgeGroupBenefits{
 		benefits.AgeGroupBenefits{
 			AgesMonths:      calc.AgeRange{0, (MonthsInYear * 6) - 1},
-			AmountsPerMonth: calc.Bracket{0, 55},
+			AmountsPerMonth: finance.Bracket{0, 55},
 		},
 	},
-	ReducerFormula: calc.WeightedBracketFormula{
-		0.0132: calc.Bracket{100000, math.Inf(1)},
+	ReducerFormula: finance.WeightedBrackets{
+		0.0132: finance.Bracket{100000, math.Inf(1)},
 	},
 }
