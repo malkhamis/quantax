140f447bbe87e4f8d3eb4a62efb9684bd08281de
Mohammad Alkhamis <malkhamis@protonmail.com>
2019-04-01 23:06:00 -0700

    Update the test and remove unneeded file

--- calc/finance/finances_test.go
+++ calc/finance/finances_test.go
@@ -4,11 +4,19 @@ import "testing"
 
 func TestFamilyFinances(t *testing.T) {
 
-	spouse1 := IndividualFinances{Income: 10, Deductions: 20}
-	spouse2 := IndividualFinances{Income: 15, Deductions: 50}
-	finances := FamilyFinances{spouse1, spouse2}
+	spouse1 := IndividualFinances{
+		Income:     IncomeBySource{IncSrcEarned: 10},
+		Deductions: DeductionBySource{DeducSrcRRSP: 20},
+	}
 
-	actualIncome := finances.Income()
+	spouse2 := IndividualFinances{
+		Income:     IncomeBySource{IncSrcEarned: 15},
+		Deductions: DeductionBySource{DeducSrcRRSP: 50},
+	}
+
+	finances := HouseholdFinances{spouse1, spouse2}
+
+	actualIncome := finances.TotalIncome()
 	expectedIncome := float64(10 + 15)
 	if actualIncome != expectedIncome {
 		t.Errorf(
@@ -17,26 +25,12 @@ func TestFamilyFinances(t *testing.T) {
 		)
 	}
 
-	actualDeductions := finances.Deductions()
+	actualDeductions := finances.TotalDeductions()
 	expectedDeductions := float64(20 + 50)
 	if actualDeductions != expectedDeductions {
 		t.Errorf(
 			"unexpected income total\nwant: %.2f\n got: %.2f",
 			expectedDeductions, actualDeductions,
-		)
-	}
-
-	actualSpouse1, actualSpouse2 := finances.Split()
-	if actualSpouse1 != spouse1 {
-		t.Errorf(
-			"actual split finances is not equal to expected\nwant: %v\n got: %v",
-			spouse1, actualSpouse1,
-		)
-	}
-	if actualSpouse2 != spouse2 {
-		t.Errorf(
-			"actual split finances is not equal to expected\nwant: %v\n got: %v",
-			spouse2, actualSpouse2,
 		)
 	}
 }
