073af5443ea647bca42ca76790919b971c613c25
Mohammad Alkhamis <malkhamis@protonmail.com>
2019-04-03 22:42:12 -0700

    Add test to ensure number of fields does not change without notice

--- calc/tax/formula_test.go
+++ calc/tax/formula_test.go
@@ -1,6 +1,7 @@
 package tax
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/malkhamis/quantax/calc/finance"
@@ -28,5 +29,18 @@ func TestFormula_Clone_Nil(t *testing.T) {
 	clone := mr.Clone()
 	if clone != nil {
 		t.Fatal("cloning a nil formula should return nil")
+	}
+}
+
+func TestFormula_NumFieldsUnchanged(t *testing.T) {
+
+	dummy := CanadianFormula{}
+	s := reflect.ValueOf(&dummy).Elem()
+	if s.NumField() != 3 {
+		t.Fatal(
+			"number of struct fields changed. Please update the constructor and the " +
+				"clone method of this type. Next, update this test with the new " +
+				"number of fields",
+		)
 	}
 }
