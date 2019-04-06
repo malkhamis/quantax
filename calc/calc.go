229fdb8ed05c8308420ea537cf1c47e2053a003e
Mohammad Alkhamis <malkhamis@protonmail.com>
2019-04-06 00:11:52 -0700

    Add a new interface in calc for income calculator

--- calc/calc.go
+++ calc/calc.go
@@ -6,6 +6,16 @@ import (
 	"github.com/malkhamis/quantax/calc/human"
 )
 
+// IncomeCalculator is used to calculate income used for benefits and tax
+type IncomeCalculator interface {
+	// TotalIncome returns the total adjusted income
+	TotalIncome(finance.IncomeDeductor) float64
+	// TotalDeductions returns total adjusted deductions
+	TotalDeductions(finance.IncomeDeductor) float64
+	// NetIncome returns total income less total deduction
+	NetIncome(finance.IncomeDeductor) float64
+}
+
 // TaxCalculator is used to calculate payable tax on individual earnings
 type TaxCalculator interface {
 	// Calc returns the payable amount of tax for the given finances
