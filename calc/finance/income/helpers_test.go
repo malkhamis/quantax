eed1526dc2e039aaf34258911dcd642743ca471c
Mohammad Alkhamis <malkhamis@protonmail.com>
2019-04-11 22:09:13 -0700

    Fix failing tests in income

--- calc/finance/income/helpers_test.go
+++ calc/finance/income/helpers_test.go
@@ -5,8 +5,10 @@ import "github.com/malkhamis/quantax/calc/finance"
 type testIncomeDeductor struct {
 	totalIncome     float64
 	totalDeductions float64
+	totalMiscAmount float64
 	incomeSrcs      finance.IncomeSourceSet
 	deducSrcs       finance.DeductionSourceSet
+	miscSrcs        finance.MiscSourceSet
 }
 
 func (tid testIncomeDeductor) TotalIncome(sources ...finance.IncomeSource) float64 {
@@ -15,11 +17,17 @@ func (tid testIncomeDeductor) TotalIncome(sources ...finance.IncomeSource) float
 func (tid testIncomeDeductor) TotalDeductions(sources ...finance.DeductionSource) float64 {
 	return tid.totalDeductions
 }
+func (tid testIncomeDeductor) MiscAmount(sources ...finance.MiscSource) float64 {
+	return tid.totalMiscAmount
+}
 func (tid testIncomeDeductor) IncomeSources() finance.IncomeSourceSet {
 	return tid.incomeSrcs
 }
 func (tid testIncomeDeductor) DeductionSources() finance.DeductionSourceSet {
 	return tid.deducSrcs
+}
+func (tid testIncomeDeductor) MiscSources() finance.MiscSourceSet {
+	return tid.miscSrcs
 }
 
 type testAdjuster struct {
