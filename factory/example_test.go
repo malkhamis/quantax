7ae73e55689c19fb64e912e98563984db82fbf30
Mohammad Alkhamis <malkhamis@protonmail.com>
2019-04-20 00:15:36 -0700

    Remove a TODO that is hard to fix in example

--- factory/example_test.go
+++ factory/example_test.go
@@ -24,8 +24,8 @@ func ExampleNewTaxFactory() {
 	finances.AddIncome(finance.IncSrcTFSA, 12000)
 	finances.AddDeduction(finance.DeducSrcRRSP, 10000)
 
-	aggTax, _ := calculator.TaxPayable() // TODO print credits
-	fmt.Printf("%.2f", aggTax)           // Output: 52821.09
+	aggTax, _ := calculator.TaxPayable()
+	fmt.Printf("%.2f\n", aggTax) // Output: 52821.09
 }
 
 func ExampleNewChildBenefitFactory() {
