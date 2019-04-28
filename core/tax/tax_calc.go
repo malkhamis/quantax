ca0fd327250b7440cdba05f344bb012812cab31c
Mohammad Alkhamis <malkhamis@protonmail.com>
2019-04-28 00:01:06 -0700

    Update the tax calculator per new interface

--- core/tax/tax_calc.go
+++ core/tax/tax_calc.go
@@ -15,8 +15,10 @@ type Calculator struct {
 	contraFormula    ContraFormula
 	incomeCalculator core.IncomeCalculator
 	finances         core.HouseholdFinances
-	credits          []*taxCredit
+	crSpouseA        []core.TaxCredit
+	crSpouseB        []core.TaxCredit
 	dependents       []*human.Person
+	taxInfo          core.TaxInfo
 }
 
 // NewCalculator returns a new tax calculator for the given tax formula and the
@@ -44,10 +46,8 @@ func NewCalculator(cfg CalcConfig) (*Calculator, error) {
 // calculations. If finances is nil or both spouses' finances are nil, a noop
 // instance is set
 func (c *Calculator) SetFinances(f core.HouseholdFinances) {
-	if f == nil {
-		f = core.NewHouseholdFinancesNop()
-	}
-	if f.SpouseA() == nil && f.SpouseB() == nil {
+
+	if f == nil || (f.SpouseA() == nil && f.SpouseB() == nil) {
 		f = core.NewHouseholdFinancesNop()
 	}
 	c.finances = f
@@ -58,20 +58,27 @@ func (c *Calculator) SetFinances(f core.HouseholdFinances) {
 // by these credits.
 func (c *Calculator) SetCredits(credits []core.TaxCredit) {
 
-	c.credits = make([]*taxCredit, 0, len(credits))
-
 	for _, cr := range credits {
 
-		if cr.Amount() == 0 {
+		if cr == nil {
 			continue
 		}
 
-		typed, ok := cr.(*taxCredit)
-		if ok && typed.owner != c {
+		if _, _, remaining := cr.Amounts(); remaining <= 0 {
 			continue
 		}
 
-		c.credits = append(c.credits, typed)
+		if cr.TaxInfo() != c.taxInfo {
+			continue
+		}
+
+		ref := cr.ReferenceFinancer()
+		if ref == c.finances.SpouseA() {
+			c.crSpouseA = append(c.crSpouseA, cr)
+		} else if ref == c.finances.SpouseB() {
+			c.crSpouseB = append(c.crSpouseB, cr)
+		}
+
 	}
 
 }
@@ -89,19 +96,24 @@ func (c *Calculator) SetDependents(dependents ...*human.Person) {
 	}
 }
 
+// TaxInfo TODO..
+func (c *Calculator) TaxInfo() []core.TaxInfo {
+	return []core.TaxInfo{c.formula.TaxInfo()}
+}
+
 // TaxPayable computes the tax on the net income for the previously set finances
 // and any relevent credits.
-func (c *Calculator) TaxPayable() (spouseA, spouseB float64, unusedCredits []core.TaxCredit) {
+func (c *Calculator) TaxPayable() (spouseA, spouseB float64, combinedCredits []core.TaxCredit) {
 
 	netIncomeA, netIncomeB := c.netIncome()
 	totalTaxA, totalTaxB := c.totalTax(netIncomeA, netIncomeB)
-	newCredits := c.totalCombinedCredits(netIncomeA, netIncomeB)
+	taxCrA, taxCrB := c.totalCredits(netIncomeA, netIncomeB)
 
-	netPayableTaxA, unusedCrA := c.netPayableTax(totalTaxA, newCredits)
-	netPayableTaxB, unusedCrB := c.netPayableTax(totalTaxB, newCredits)
-	unusedCr := taxCreditGroup(append(unusedCrA, unusedCrB...)).typecast()
+	netPayableTaxA := c.netPayableTax(totalTaxA, taxCrA)
+	netPayableTaxB := c.netPayableTax(totalTaxB, taxCrB)
+	finalCr := append(taxCrA, taxCrB...)
 
-	return netPayableTaxA, netPayableTaxB, unusedCr
+	return netPayableTaxA, netPayableTaxB, finalCr
 }
 
 // netIncome returns the net income for both spouses in the set finances
@@ -123,101 +135,78 @@ func (c *Calculator) totalTax(netIncomeA, netIncomeB float64) (totalTaxA, totalT
 	return totalTaxA, totalTaxB
 }
 
-// totalCombinedCredits returns tax credits for the set household finances. Each
-// individual tax credit instance is associated with its own financer
-func (c *Calculator) totalCombinedCredits(netIncomeA, netIncomeB float64) []*taxCredit {
+func (c *Calculator) totalCredits(netIncomeA, netIncomeB float64) (totalCrA, totalCrB []core.TaxCredit) {
 
 	taxPayerA, taxPayerB := c.makeTaxPayers(netIncomeA, netIncomeB)
-	creditsA := c.taxCredits(taxPayerA)
-	creditsB := c.taxCredits(taxPayerB)
 
-	allCredits := append(c.credits, creditsA...)
-	allCredits = append(allCredits, creditsB...)
-	return allCredits
+	newCrA := taxCreditGroup(
+		c.contraFormula.Apply(taxPayerA),
+	).typecast()
+
+	newCrB := taxCreditGroup(
+		c.contraFormula.Apply(taxPayerB),
+	).typecast()
+
+	totalCrA = append(c.crSpouseA, newCrA...)
+	totalCrB = append(c.crSpouseB, newCrB...)
+
+	c.contraFormula.FilterAndSort(totalCrA)
+	c.contraFormula.FilterAndSort(totalCrB)
+
+	return totalCrA, totalCrB
 }
 
-// taxCredits returns the tax credits for the given tax payer. individual tax
-// credits are associated with this calculator and tax payer's finanaces. If
-// a tax credit is nil or its amount is zero, it is skipped. If the given tax
-// payer is nil, it returns nil
-func (c *Calculator) taxCredits(taxPayer *TaxPayer) []*taxCredit {
+// netPayableTax returns the payable tax after using the given credits. It also
+// sets new values for the given tax credits according to how they were used
+func (c *Calculator) netPayableTax(taxAmount float64, credits []core.TaxCredit) float64 {
 
-	if taxPayer == nil {
-		return nil
-	}
+	for _, cr := range credits {
 
-	allCredits := c.contraFormula.Apply(taxPayer)
-	usableCredits := make([]*taxCredit, 0, len(allCredits))
-	for _, cr := range allCredits {
+		ruleType := cr.Rule().Type
+		initial, used, remaining := cr.Amounts()
 
-		if cr == nil {
-			continue
-		}
-		if cr.amount == 0 {
+		if taxAmount <= 0.0 && ruleType == CrRuleTypeCanCarryForward {
 			continue
 		}
 
-		cr.owner = c
-		cr.ref = taxPayer.Finances
-
-		usableCredits = append(usableCredits, cr)
-	}
-
-	return usableCredits
-}
-
-// netPayableTax returns the payable tax and any unsable/remaining credits. It
-// assumes that the given credits are owned by this calculator
-func (c *Calculator) netPayableTax(taxAmount float64, credits []*taxCredit) (float64, []*taxCredit) {
-
-	newCredits := taxCreditGroup(credits).clone()
-
-	for _, cr := range newCredits {
-
-		if taxAmount <= 0.0 && cr.rule.Type == CrRuleTypeCanCarryForward {
+		if taxAmount <= 0.0 && ruleType == CrRuleTypeNotCarryForward {
+			cr.SetAmounts(initial, used, 0)
 			continue
 		}
 
-		if taxAmount <= 0.0 && cr.rule.Type == CrRuleTypeNotCarryForward {
-			cr.amount = 0
+		if taxAmount >= remaining || ruleType == CrRuleTypeCashable {
+			taxAmount -= remaining
+			cr.SetAmounts(initial, used+remaining, 0)
 			continue
 		}
 
-		if taxAmount >= cr.amount || cr.rule.Type == CrRuleTypeCashable {
-			taxAmount -= cr.amount
-			cr.amount = 0
-			continue
+		// reached at most once: 0 < taxAmount < remaining
+		if ruleType == CrRuleTypeNotCarryForward {
+			cr.SetAmounts(initial, used+taxAmount, 0)
+		} else {
+			cr.SetAmounts(initial, used+taxAmount, remaining-taxAmount)
 		}
-
-		// reached at most once
-		diff := cr.amount - taxAmount
 		taxAmount = 0.0
-		cr.amount = diff
-		if cr.rule.Type == CrRuleTypeNotCarryForward {
-			cr.amount = 0.0
-		}
+
 	}
 
-	return taxAmount, newCredits
+	return taxAmount
 }
 
 // makeTaxPayers returns dual tax payers from the given net income amounts and
 // the finances stored in this calculator
-func (c *Calculator) makeTaxPayers(netIncomeA, netIncomeB float64) (A, B *TaxPayer) {
+func (c *Calculator) makeTaxPayers(netIncomeA, netIncomeB float64) (taxPayerA, taxPayerB *TaxPayer) {
 
-	var (
-		taxPayerA, taxPayerB *TaxPayer
-		financesA            = c.finances.SpouseA()
-		financesB            = c.finances.SpouseB()
-	)
+	financesA := c.finances.SpouseA()
+	financesB := c.finances.SpouseB()
 
 	if financesA != nil {
 		taxPayerA = &TaxPayer{
 			Finances:        financesA,
 			NetIncome:       netIncomeA,
-			Dependents:      c.dependents,
-			HasSpouse:       financesB != nil,
+			SpouseFinances:  financesB,
 			SpouseNetIncome: netIncomeB,
+			Dependents:      c.dependents,
 		}
 	}
 
@@ -225,9 +214,9 @@ func (c *Calculator) makeTaxPayers(netIncomeA, netIncomeB float64) (A, B *TaxPay
 		taxPayerB = &TaxPayer{
 			Finances:        financesB,
 			NetIncome:       netIncomeB,
-			Dependents:      c.dependents,
-			HasSpouse:       financesA != nil,
+			SpouseFinances:  financesA,
 			SpouseNetIncome: netIncomeA,
+			Dependents:      c.dependents,
 		}
 	}
 
