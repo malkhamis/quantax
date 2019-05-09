8aea7c2a66f8b2d85948420b774b4cf5a22a312c
Mohammad Alkhamis <malkhamis@protonmail.com>
2019-05-09 08:45:15 -0700

    Remove the junk in cmd.go and add a useful code for toying around

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,40 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/malkhamis/quantax/core"
+	"github.com/malkhamis/quantax/factory"
+)
+
 func main() {
 
-	// config := factory.RRSPFactoryConfig{
-	// 	Year:       2018,
-	// 	RRSPRegion: factory.Canada,
-	// 	TaxRegions: []factory.Region{factory.Canada, factory.BC},
-	// }
-	//
-	// f := factory.NewRRSPFactory(config)
-	// c, err := f.NewCalculator()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	os.Exit(1)
-	// }
-	//
-	// finances := finance.IndividualFinances{
-	// 	Income:   83000,
-	// 	RRSPRoom: 22000,
-	// }
-	// c.SetFinances(finances)
-	//
-	// var contribution float64
-	// for {
-	//
-	// 	contribution += 500
-	// 	refund, err := c.TaxRefund(contribution)
-	// 	if err != nil {
-	// 		return
-	// 	}
-	//
-	// 	fmt.Printf("Contribution: %.2f\tROI:\t%.2f\n", contribution, refund/contribution)
-	// }
-}
+	initAmounts := map[core.FinancialSource]float64{
+		core.IncSrcEligibleDividendsCA: 119000,
+		core.MiscSrcTuition:            0,
+	}
+	ff := factory.NewFinanceFactory()
+	myFinances := ff.NewHouseholdFinancesForSingle(initAmounts)
 
-// func iter(n int) []struct{} {
-// 	s := make([]struct{}, n)
-// 	return s
-// }
+	tf := factory.NewTaxFactory(2018, core.RegionCA, core.RegionBC)
+	calc, err := tf.NewCalculator()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	calc.SetFinances(myFinances, nil)
+	taxPaid, _, credits := calc.TaxPayable()
+	fmt.Println(taxPaid)
+
+	for _, cr := range credits {
+		fmt.Println(cr.Source(), cr.Region())
+		initial, used, remain := cr.Amounts()
+		fmt.Printf("initial: %.2f\tused: %.2f\tremaining: %.2f\n\n", initial, used, remain)
+	}
+}
