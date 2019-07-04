870e48e5e7dfd9727e5db58a66eb1fd411a78faf
Mohammad Alkhamis <malkhamis@protonmail.com>
2019-07-04 08:57:30 -0700

    Remove prototype code and other empty files

--- core/human/human.go
+++ core/human/human.go
@@ -1,2 +1,5 @@
-// Package human provide basic tools to represent tax payers and beneficiaries
+// Package human provide basic tools to represent tax payers and beneficiaries.
+// TODO
+// This package seems unnecessary, but moving the code to package 'core' makes
+// things worse as we don't want details to spill to the layer of business rules
 package human
